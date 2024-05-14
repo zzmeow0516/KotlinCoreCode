bf164a339dfb433e876afadfeaa2ee2675b40a49
zzmeow0516 <zzmeow0516@163.com>
2024-05-14 22:36:51 +0800

    val, var

--- app/src/main/java/com/example/kotlincorecode/CoreCode.kt
+++ app/src/main/java/com/example/kotlincorecode/CoreCode.kt
@@ -2,6 +2,15 @@ package com.example.kotlincorecode
 
 
 public val TAG = "CoreCode"
+
+class Book(var name: String) {
+    fun printName() {
+        println(this.name)
+    }
+}
+
 fun main() {
-    println("wow")
+    val book1 = Book("Kotlin Core Code")
+    book1.name = "Kotlin code"
+    book1.printName()
 }
